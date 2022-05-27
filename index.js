0eede0010b146de5267a2957bc0da891f91a7d02
SabbirSami <smd71430@gmail.com>
2022-05-27 17:30:18 +0600

    simple setup done

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 const express = require("express");
+const port = process.env.PORT || 5000;
+const cors = require("cors");
+const app = express();
+require("dotenv").config();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.send("To Do World!");
+});
+
+app.listen(port, () => {
+    console.log(`listening on port ${port}`);
+});
