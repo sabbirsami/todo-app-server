328b7f0e636a527707e0e7048297732c04a788c8
SabbirSami <smd71430@gmail.com>
2022-05-27 22:14:06 +0600

    delete api added

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const port = process.env.PORT || 5000;
 const cors = require("cors");
 const app = express();
 require("dotenv").config();
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
 app.use(cors());
 app.use(express.json());
@@ -19,14 +19,26 @@ async function run() {
     try {
         await client.connect();
         const todoCollection = client.db("toDoNotes").collection("notes");
+
+        // TO GET NOTE FROM USER
         app.post("/note", async (req, res) => {
             const note = req.body;
             const result = await todoCollection.insertOne(note);
             res.send(result);
         });
+
+        // TO GET ALL USER NOTE
         app.get("/note", async (req, res) => {
             const notes = await todoCollection.find().toArray();
             res.send(notes);
+        });
+
+        //TO DELETE SELECTED NOTE
+        app.delete("/note", async (req, res) => {
+            const id = req.params.deletedId;
+            const query = { _id: ObjectId(id) };
+            const result = await todoCollection.deleteOne(query);
+            res.send(result);
         });
     } finally {
     }
